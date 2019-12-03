6364791a570f37e3b0ca8c27a1af03ce62654af1
ErickStone <erick.stone@usu.edu>
2019-12-02 20:57:36 -0700

    problem 5 progress

--- HW6/problem_5.py
+++ HW6/problem_5.py
@@ -1,14 +1,44 @@
-from sklearn.linear_model import LogisticRegression
+from scipy.io import loadmat
 from sklearn.model_selection import LeaveOneOut, cross_val_score
+from sklearn.neighbors import KernelDensity
+import numpy as np
+pi_2_sqrt = np.sqrt(np.pi * 2)
 
 
 def get_data():
-    return 1, 2
+    dict = loadmat('anomaly.mat')
+    # get x,y
+    x = dict['x']
+    y = dict['y']
+    return x, y
+
+
+def gau(x, bandwidth):
+    return np.exp(-x**2/(2*bandwidth**2))/(bandwidth*pi_2_sqrt)
+
+
+def test_kde(x, y):
+    loocv = LeaveOneOut()
+    loocv.get_n_splits(x)
+
+    bandwidth = 1.0
+    model = KernelDensity(bandwidth=bandwidth, kernel='gaussian')
+    res = cross_val_score(model, x, y, cv=loocv)
+    print(bandwidth)
+
+
+def kde(x, y, bandwidth):
+    kde_v = gau(x, bandwidth)/len(x)
+
 
 def run():
     # Part A
     train_x, train_y = get_data()
-    loocv = LeaveOneOut()
-    n_split = loocv.get_n_splits(train_x)
-    model = LogisticRegression()
-    res = cross_val_score(model, train_x, train_y, cv=loocv)
+    test_kde(train_x, train_y)
+
+
+    # Part B
+
+    # Part C
+    # The outlier score
+    #
