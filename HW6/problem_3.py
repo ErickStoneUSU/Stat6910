70516b3849d9b06449cda1fd3d7c02c514e90098
Erick Stone <Erick.Stone@usu.edu>
2019-12-05 10:14:48 -0700

    5 done, 3 not working, but close

--- HW6/problem_3.py
+++ HW6/problem_3.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pylab as plt
+from scipy.stats import norm
 
 np.random.seed(0)
 n = 200
 K = 2
-e = np.array([0.7, 0.3])
-w = np.array([-2, 1])
-b = np.array([0.5, 0.5])
-v = np.array([0.2, 0.1])
+e = np.array([0.5, 0.5])  #mixing weight
+w = np.array([1, -1]) #slopes of line
+b = np.array([0.0, 0.0]) #offsets of line
+v = np.array([0.5, 0.5]) #variance
 x = np.zeros([n])
 y = np.zeros([n])
 for i in range(0, n):
@@ -21,14 +22,40 @@ t = np.linspace(0, 1, num=100)
 plt.plot(t, w[0]*t+b[0], 'k')
 plt.plot(t, w[1]*t+b[1], 'k')
 plt.show()
-# TODO implement init variables
+
+# TODO double check v is sample variance of y data
 # TODO implement log-likelihood
 # TODO implement plots
 
 prev = 0.0
 curr = 1.0
-while abs(prev - curr) > 0.0:
-    # E Step
-    print('a')
-    # M Step
-    print('b')
+itt = 0
+ll = []
+sig = [0.5, 0.5]
+mu = [np.var(y), np.var(y)]
+
+while abs(curr - prev) > 0.0001:
+    prev = curr
+
+    # E Step - this is where we update the weights and biases
+    b[0] = np.sum(w[0] * x) / (K * n)
+    b[1] = np.sum(w[1] * x) / (K * n)
+
+    w[0] = np.sum(norm(mu[0], sig[0]).pdf(x)) * e[0]
+    w[1] = np.sum(norm(mu[1], sig[1]).pdf(x)) * e[1]
+
+    # M Step - this is where we update the hypothesis
+    e = w / n
+    mu = np.sum(w.dot(x)) / np.sum(w)
+    sig = np.sum(w.dot((x-mu)**2))
+
+    itt += 1
+    curr = np.log(sum(w))
+    ll.append(abs(curr))
+
+print('iterations: ' + str(itt))
+# plot ll to itt
+print('W: ' + str(w))
+print('b: ' + str(b))
+# plot showing data, true lines, estimated lines
+
--- HW6/problem_5.py
+++ HW6/problem_5.py
@@ -1,44 +1,99 @@
 from scipy.io import loadmat
-from sklearn.model_selection import LeaveOneOut, cross_val_score
+from sklearn.model_selection import LeaveOneOut, cross_val_score, GridSearchCV
 from sklearn.neighbors import KernelDensity
 import numpy as np
+import matplotlib.pyplot as plt
 pi_2_sqrt = np.sqrt(np.pi * 2)
 
 
 def get_data():
     dict = loadmat('anomaly.mat')
     # get x,y
-    x = dict['x']
-    y = dict['y']
-    return x, y
+    x = dict['X']
+    x_t1 = dict['xtest1']
+    x_t2 = dict['xtest2']
+    return x.T, x_t1.T, x_t2.T
 
 
 def gau(x, bandwidth):
     return np.exp(-x**2/(2*bandwidth**2))/(bandwidth*pi_2_sqrt)
 
 
-def test_kde(x, y):
+def part_a(x):
     loocv = LeaveOneOut()
     loocv.get_n_splits(x)
 
-    bandwidth = 1.0
-    model = KernelDensity(bandwidth=bandwidth, kernel='gaussian')
-    res = cross_val_score(model, x, y, cv=loocv)
-    print(bandwidth)
+    bandwidths = 10 ** np.linspace(-1, 1, 100)
+    grid = GridSearchCV(KernelDensity(kernel='gaussian'),
+                        {'bandwidth': bandwidths},
+                        cv=loocv)
+    grid.fit(x)
+    print(grid.best_params_)
+
+
+def part_b(x):
+    kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(x)
+    samps = np.random.uniform(-2, 4, 1000)
+    dens = kde.score_samples(samps.reshape(-1,1))
+    plt.plot(samps, dens)
+    plt.show()
+    print('b')
+
+
+def part_d_test(x, p1, p2):
+    kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(x)
+    sc1 = kde.score(p1)
+    sc2 = kde.score(p2)
+    sc_b = cross_val_score(kde, x)
+    sc1_b = sc1 / (sum(sc_b) / len(sc_b))
+    sc2_b = sc2 / (sum(sc_b) / len(sc_b))
+    print(sc1_b)
+    print(sc2_b)
+    print('c')
+
+
+def get_score1(x, p):
+    # get the f'h(x) :: KDE
+    # k_h: bandwidth^-d k (y/bandwidth)
+    k_h = gau(x, .1)
+    # KDE: 1/n * sum(k_h(X-x))
+    kde = 1/len(x) * np.sum(k_h * (x - p))
+
+    # get the average of all the points except the one you are testing
+    av = sum(x) / len(x)
+    # divide the f'h(x) by the average to get the outlier score
+    return np.abs(kde / av)
+
+
+def part_d(x, p1, p2):
+    print(get_score1(x, p1))
+    print(get_score1(x, p2))
 
 
 def kde(x, y, bandwidth):
     kde_v = gau(x, bandwidth)/len(x)
 
 
+def get_score2(x, p, k):
+    # get k nearest neighbors
+    # divide dist to furthest knn by ave dist to knn
+    dist = [np.abs(i - p) for i in x]
+    dist = sorted(dist)[:k]
+    return dist[-1] / (sum(dist) / k)
+
+
+def part_f(x, p1, p2, k):
+    print([k, get_score2(x, p1, k)])
+    print([k, get_score2(x, p2, k)])
+
+
 def run():
     # Part A
-    train_x, train_y = get_data()
-    test_kde(train_x, train_y)
+    x, x_t1, x_t2 = get_data()
+    # part_a(x) # get the bandwidth
+    # part_b(x) # plot the kde
+    for i in [100, 150, 200]:
+        part_f(x, x_t1, x_t2, i)  # The outlier score
 
 
-    # Part B
-
-    # Part C
-    # The outlier score
-    #
+run()
